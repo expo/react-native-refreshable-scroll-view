a1b8728afe5d9f17ed47a187aabbd54a5c11af8c
James Ide <ide@jameside.com>
2015-09-30 14:05:46 -0700

    [Interaction] Greatly reduce jankiness when restoring the scroll position
    Rewrote the part for restoring the scroll position after refreshing is completed. This mimics the Twitter app in this way where the scroll view is non-interactive while it slides back up from its resting position w/the refresh indicator to its resting position without it. This is feeling close to production quality.

--- RefreshableScrollView.js
+++ RefreshableScrollView.js
@@ -17,6 +17,8 @@ let cloneReferencedElement = require('react-native-clone-referenced-element');
 
 let RefreshIndicator = require('./RefreshIndicator');
 
+const SCROLL_ANIMATION_DURATION_MS = 350;
+
 let RefreshableScrollView = React.createClass({
   mixins: [ScrollableMixin, TimerMixin],
 
@@ -38,10 +40,11 @@ let RefreshableScrollView = React.createClass({
   getInitialState() {
     return {
       tracking: false,
-      trackingAfterBeganRefreshing: false,
-      trackingSinceEndedRefreshing: false,
       pullToRefreshProgress: 0,
       refreshing: false,
+      waitingToRest: false,
+      returningToTop: false,
+      shouldIncreaseContentInset: false,
       refreshIndicatorEnd: null,
     };
   },
@@ -78,7 +81,7 @@ let RefreshableScrollView = React.createClass({
     }
 
     let isRefreshIndicatorActive =
-      this.state.refreshing || this.state.trackingSinceEndedRefreshing;
+      this.state.refreshing || this.state.waitingToRest;
     if (!isRefreshIndicatorActive && this.state.pullToRefreshProgress <= 0) {
       refreshIndicatorStyle.opacity = 0;
     }
@@ -89,11 +92,15 @@ let RefreshableScrollView = React.createClass({
     });
 
     let scrollComponent = renderScrollComponent({
+      pointerEvents: this.state.returningToTop ? 'none' : 'auto',
       ...scrollViewProps,
       contentInset: this._getContentInsetAdjustedForIndicator(),
       onResponderGrant: this._handleResponderGrant,
       onResponderRelease: this._handleResponderRelease,
       onScroll: this._handleScroll,
+      onStartShouldSetResponder: (event) => { console.log('onstart'); return false; },
+      onScrollShouldSetResponder: (event) => { console.log('onscroll'); return false; },
+      onMomentumScrollEnd: this._handleMomentumScrollEnd,
       style: styles.scrollComponent,
     });
     scrollComponent = cloneReferencedElement(scrollComponent, {
@@ -115,12 +122,10 @@ let RefreshableScrollView = React.createClass({
 
   _getContentInsetAdjustedForIndicator() {
     let { contentInset, horizontal } = this.props;
-    let { refreshing, tracking, trackingAfterBeganRefreshing } = this.state;
+    let { shouldIncreaseContentInset } = this.state;
 
-    let shouldAccomodateIndicator =
-      refreshing && (!tracking || trackingAfterBeganRefreshing) ||
-      !refreshing && trackingAfterBeganRefreshing;
-    if (!shouldAccomodateIndicator) {
+    if (!shouldIncreaseContentInset) {
+      console.log('not adjust');
       return contentInset;
     }
 
@@ -136,28 +141,33 @@ let RefreshableScrollView = React.createClass({
         contentInset.top != null ? contentInset.top : 0
       );
     }
+    console.log('adjust');
     return contentInset;
+  },
+
+  _isOverscrolled() {
+    let { x, y } = this._nativeContentOffset;
+    let distanceFromTop = this.props.horizontal ?
+      x + this._nativeContentInset.left :
+      y + this._nativeContentInset.top;
+    return distanceFromTop < 0;
   },
 
   _handleResponderGrant(event) {
     if (this.props.onResponderGrant) {
       this.props.onResponderGrant(event);
     }
-    this.setState(state => ({
-      tracking: true,
-      trackingAfterBeganRefreshing: state.refreshing,
-    }));
+    this.setState({ tracking: true });
   },
 
   _handleResponderRelease(event) {
     if (this.props.onResponderRelease) {
       this.props.onResponderRelease(event);
     }
-    this.setState({
+    this.setState(state => ({
       tracking: false,
-      trackingAfterBeganRefreshing: false,
-      trackingSinceEndedRefreshing: false,
-    });
+      shouldIncreaseContentInset: state.refreshing || state.waitingToRest,
+    }));
   },
 
   _handleScroll(event) {
@@ -166,6 +176,7 @@ let RefreshableScrollView = React.createClass({
     }
 
     let { contentInset, contentOffset } = event.nativeEvent;
+    this._nativeContentInset = contentInset;
     this._nativeContentOffset = contentOffset;
     this._nativeContentInsetAdjustment =
       this._calculateNativeContentInsetAdjustment(contentInset);
@@ -192,9 +203,10 @@ let RefreshableScrollView = React.createClass({
 
     let wasRefreshing;
     this.setState(state => {
-      wasRefreshing = state.refreshing;
+      let { tracking, refreshing, waitingToRest, returningToTop } = state;
+      wasRefreshing = refreshing;
       let shouldBeginRefreshing = (pullToRefreshProgress === 1) &&
-        state.tracking && !state.trackingSinceEndedRefreshing;
+        tracking && !refreshing && !waitingToRest && !returningToTop;
       return {
         pullToRefreshProgress,
         refreshing: state.refreshing || shouldBeginRefreshing,
@@ -221,27 +233,73 @@ let RefreshableScrollView = React.createClass({
     return adjustment;
   },
 
+  _handleMomentumScrollEnd(event) {
+    if (this.props.onMomentumScrollEnd) {
+      this.props.onMomentumScrollEnd(event);
+    }
+    if (this.state.waitingToRest) {
+      this._restoreScrollView();
+    }
+  },
+
   _handleRefreshEnd() {
     if (!this.state.refreshing) {
       return;
     }
 
-    // if (!this.state.tracking) {
-    //   let { x, y } = this._nativeContentOffset;
-    //   let { horizontal, contentInset } = this.props;
-    //   let contentInsetWithIndicator = this._scrollComponent.props.contentInset;
-    //   if (horizontal) {
-    //     let delta = contentInsetWithIndicator.left - contentInset.left;
-    //     this.scrollTo(y, x + delta);
-    //   } else {
-    //     let delta = contentInsetWithIndicator.top - contentInset.top;
-    //     this.scrollTo(y + delta, x);
-    //   }
-    // }
+    // Let the scroll view naturally bounce back to its resting position before
+    // hiding the loading indicator if it is still pulled down or the user is
+    // touching it
+    let waitingToRest = this.state.tracking || this._isOverscrolled();
+    this.setState({
+      refreshing: false,
+      waitingToRest,
+    });
+
+    if (!waitingToRest) {
+      this._restoreScrollView();
+    }
+  },
+
+  _restoreScrollView() {
+    // Scroll up to the top to restore the scrollable content's position
+    let scrollDestination = null;
+    let { x, y } = this._nativeContentOffset;
+    let { horizontal, contentInset } = this.props;
+    let contentInsetWithIndicator = this._scrollComponent.props.contentInset;
+    if (horizontal) {
+      let indicatorWidth = contentInsetWithIndicator.left - contentInset.left;
+      let scrolledDistance = this._nativeContentInset.left + x;
+      if (indicatorWidth > 0 && indicatorWidth > scrolledDistance) {
+        let destinationX = Math.min(x, -this._nativeContentInset.left) + indicatorWidth;
+        scrollDestination = [y, destinationX];
+      }
+    } else {
+      let indicatorHeight = contentInsetWithIndicator.top - contentInset.top;
+      let scrolledDistance = this._nativeContentInset.top + y;
+      if (indicatorHeight > 0 && indicatorHeight > scrolledDistance) {
+        let destinationY = Math.min(y, -this._nativeContentInset.top) + indicatorHeight;
+        scrollDestination = [destinationY, x];
+      }
+    }
 
     this.setState({
       refreshing: false,
-      trackingSinceEndedRefreshing: this.state.tracking,
+      waitingToRest: false,
+      returningToTop: !!scrollDestination,
+      shouldIncreaseContentInset: false,
+    }, () => {
+      if (scrollDestination) {
+        this.scrollTo(...scrollDestination);
+        // We detect whether the scrolling has finished based on the scroll
+        // position, but we must eventually set returningToTop to false since
+        // we block user interactions while it is true
+        this.clearTimeout(this._returningToTopSafetyTimeout);
+        this._returningToTopSafetyTimeout = this.setTimeout(() => {
+          this._returningToTopSafetyTimeout = null;
+          this.setState({ returningToTop: false });
+        }, SCROLL_ANIMATION_DURATION_MS);
+      }
     });
   },
 
